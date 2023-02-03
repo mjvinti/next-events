a58d68f4145b6826176520db926338dea0ff6cfe
Mario Vinti <mjvinti@gmail.com>
2023-02-03 17:20:36 -0500

    added event detail page with dynamic routing

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,36 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "@/dummy-data";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+
 function EventDetailPage() {
+  const {
+    query: { eventId },
+  } = useRouter();
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  const { date, description, image, location, title } = event;
+
   return (
-    <div>
-      <h1>EventDetailPage</h1>
-    </div>
+    <>
+      <EventSummary title={title} />
+      <EventLogistics
+        address={location}
+        date={date}
+        image={image}
+        imageAlt={title}
+      />
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
+    </>
   );
 }
 
