4e29f5678d5898d16a194f02413ed2aedd6a8f0b
Mario Vinti <mjvinti@gmail.com>
2023-02-03 17:35:39 -0500

    added general layout wrapper component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import Layout from "@/components/layout/layout";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "@/styles/globals.css";
+
+function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default App;
