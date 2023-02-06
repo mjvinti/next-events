93e7067c89680d133e9e1fc35280a7c64c22e8ae
Mario Vinti <mjvinti@gmail.com>
2023-02-06 09:00:58 -0500

    extracting data on the catch all page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,37 @@
+import { useRouter } from "next/router";
+
+import { getFilteredEvents } from "@/dummy-data";
+
 function FilteredEventsPage() {
+  const { query } = useRouter();
+
+  const filteredData = query.slug;
+
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const [year, month] = filteredData;
+  const numYear = +year,
+    numMonth = +month;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || !filteredEvents.length) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>FilteredEventsPage</h1>
