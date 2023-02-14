11cba92cafd251f1199691f4e643096560085375
Mario Vinti <mjvinti@gmail.com>
2023-02-14 16:02:19 -0500

    added general title and decsription in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ function App({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>NextJs Events</title>
+        <meta name="description" content="NextJs Events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
