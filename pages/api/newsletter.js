a8efda0da1481151bebc8831588be4cac99f6c2f
Mario Vinti <mjvinti@gmail.com>
2023-03-03 16:56:19 -0500

    added error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,14 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  return await MongoClient.connect(process.env.DB_URL);
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  return await db.collection("newsletters").insertOne(document);
+}
+
 async function handler(req, res) {
   const {
     body: { email },
@@ -11,10 +20,21 @@ async function handler(req, res) {
       return res.status(422).json({ message: "Invalid email address." });
     }
 
-    const client = await MongoClient.connect(process.env.DB_URL);
-    const db = client.db();
-    await db.collection("newsletters").insertOne({ email });
-    client.close();
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Connecting to the database failed!" });
+    }
+
+    try {
+      await insertDocument(client, { email });
+      client.close();
+    } catch (error) {
+      return res.status(500).json({ message: "Inserting data failed!" });
+    }
 
     return res.status(201).json({ message: "Signed up!" });
   }
