7c58ed23ddcf5f4c3a4169c6b4af7ab22a3b03dd
Mario Vinti <mjvinti@gmail.com>
2023-03-07 15:24:47 -0500

    added notification context state

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,14 +1,31 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const NotificationContext = createContext({
   hideNotification: function () {},
   notification: null,
-  showNotification: function () {},
+  showNotification: function (notificationData) {},
 });
 
 export function NotificationContextProvider({ children }) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>{children}</NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
+      {children}
+    </NotificationContext.Provider>
   );
 }
 
